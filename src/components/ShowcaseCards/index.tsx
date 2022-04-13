d01419826f62caf8ec579176db91ee57d9f42af3
snowdot <co.pascu@gmail.com>
2022-04-13 20:40:44 +0300

    updated description for showcase projects

--- src/components/ShowcaseCards/index.tsx
+++ src/components/ShowcaseCards/index.tsx
@@ -14,49 +14,49 @@ const ShowcaseList: ShowcaseItem[] = [
   {
     title: 'Light.so',
     srcPath: require('@site/static/img/docs/get-inspired/light.jpg').default,
-    description: 'Light is a Metaverse Explorer, enabling users to discover connections that was not possible before.',
+    description: 'Light.so is a Metaverse Explorer enabling users to curate, discover, and explore meaningful interactions in a way that was not possible before.',
     url: 'https://light.so/'
   },
   {
     title: 'Grape.art',
     srcPath: require('@site/static/img/docs/get-inspired/grape.png').default,
-    description: 'Grape Protocol is a toolset for building token-based communities on Solana.',
+    description: 'Grape.art is a toolkit for building token-based communities on the Solana Blockchain.',
     url: 'https://grape.art/'
   },
   {
     title: 'Hello World',
     srcPath: require('@site/static/img/docs/get-inspired/helloworld.png').default,
-    description: 'Hello World is a social connection web application that allows users to discover their Ethereum network.',
+    description: 'Hello World is a web application enabling users to discover their social network on the Ethereum Blockchain.',
     url: 'https://helloworld.social/'
   },
   {
     title: 'Etherloop',
     srcPath: require('@site/static/img/docs/get-inspired/etherloop.jpg').default,
-    description: 'Etherloop is a social app that allows users to connect and track balances, tokens, transactions, POAPs and NFTs.',
+    description: 'Etherloop is a web application enabling users to build social connections and track their own tokens, balances, transactions, NFTs, and POAPs.',
     url: 'https://etherloop.moonlabs.xyz/'
   },
   {
     title: 'Galaxy ID',
     srcPath: require('@site/static/img/docs/get-inspired/projectgalaxy.png').default,
-    description: 'Project Galaxy is the largest Web3 credential data network in the world.',
+    description: 'Project Galaxy is the largest web3 credential data network.',
     url: 'https://galaxy.eco/'
   },
   {
     title: 'Unipass',
     srcPath: require('@site/static/img/docs/get-inspired/unipass.png').default,
-    description: 'UniPass is a multi-chain unified crypto identity, the carrier for the concept of \'people\' in Metaverse.',
+    description: 'UniPass is a multi-chain unified crypto identity—the carrier for the concept of \'people\' in Metaverse.',
     url: 'https://www.unipass.id/'
   },
   {
     title: 'Treasureland',
     srcPath: require('@site/static/img/docs/get-inspired/treasureland.png').default,
-    description: 'Treasureland is a cross-chain NFT platform for NFT issuance, NFT trading, NFT collections and tailored in-shop services.',
+    description: 'Treasureland is a cross-chain NFT platform for NFT issuance, trading, collection, and tailored in-shop services.',
     url: 'https://treasureland.market/'
   },
   {
     title: 'Convo Space',
     srcPath: require('@site/static/img/docs/get-inspired/convospace.png').default,
-    description: 'Convo offers all the Tooling and Infrastructure to build Social Platforms that are composable across the web, powered by Web3.',
+    description: 'Convo Space offers one-stop tooling and infrastructure to build social platforms composable across web3.',
     url: 'https://theconvo.space/'
   },
 ];
