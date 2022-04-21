8b76f08e765eba9e5b8cca854899d1eb3ae6dff0
snowdot <co.pascu@gmail.com>
2022-04-21 18:09:43 +0300

    added metadata

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,13 +1,13 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
-const math = require('remark-math');
-const katex = require('rehype-katex');
+const math = require("remark-math");
+const katex = require("rehype-katex");
 
-/** @type {import('@docusaurus/types').Config} */
+/** @type {import("@docusaurus/types").Config} */
 const config = {
   title: "CyberConnect Dev Center",
   tagline: "Building the composable social graph protocol for Web3",
@@ -21,39 +21,39 @@ const config = {
 
   presets: [
     [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
+      "classic",
+      /** @type {import("@docusaurus/preset-classic").Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/cyberconnecthq/cyberconnect-docs-v2',
-          routeBasePath: '/',
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/cyberconnecthq/cyberconnect-docs-v2",
+          routeBasePath: "/",
           remarkPlugins: [
-            [require('@docusaurus/remark-plugin-npm2yarn'), {sync: true}],
+            [require("@docusaurus/remark-plugin-npm2yarn"), {sync: true}],
             math
           ],
           rehypePlugins: [katex],
         },
         blog: false,
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
   ],
   themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    /** @type {import("@docusaurus/preset-classic").ThemeConfig} */
     ({
       navbar: {
-        title: 'CyberConnect Dev Center',
+        title: "CyberConnect Dev Center",
         logo: {
-          alt: 'CyberConnect Logo',
-          src: 'img/logo-white.svg',
+          alt: "CyberConnect Logo",
+          src: "img/logo-white.svg",
         },
         items: [],
       },
       footer: {
-        style: 'light',
+        style: "light",
         links: [],
         copyright: `Copyright © ${new Date().getFullYear()} CyberConnect.`,
       },
@@ -62,17 +62,47 @@ const config = {
         darkTheme: darkCodeTheme,
       },
       colorMode: {
-        defaultMode: 'light',
+        defaultMode: "light",
         disableSwitch: true,
+      },
+      themeConfig: {
+        metadata: [
+          // Keywords
+          { name: "keywords", content: "cyberconnect, protocol, social graph, blockchain, web3" },
+          // OG
+          { name: "og:title", content: "CyberConnect Developer Center" },
+          {
+            name: "og:description",
+            content: "The comprehensive guide for CyberConnect protocol.",
+          },
+          { name: "og:type", content: "website" },
+          { name: "og:url", content: "https://docs.cyberconnect.me/" },
+          {
+            name: "og:image",
+            content: "https://cyberconnect.me/assets/logo-black.svg",
+          },
+          // Twitter
+          { name: "twitter:title", content: "CyberConnect Developer Center" },
+          {
+            name: "twitter:description",
+            content: "The comprehensive guide for CyberConnect protocol.",
+          },
+          { name: "twitter:card", content: "summary" },
+          { name: "twitter:site", content: "@CyberConnectHQ" },
+          {
+            name: "twitter:image",
+            content: "https://cyberconnect.me/assets/logo-black.svg",
+          },
+        ],
       },
     }),
   stylesheets: [
     {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-      type: 'text/css',
+      href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
+      type: "text/css",
       integrity:
-        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-      crossorigin: 'anonymous',
+        "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
+      crossorigin: "anonymous",
     },
   ],
 };
