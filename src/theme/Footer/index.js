7679ef59798991a9ee7658be92f96482e9eac657
snowdot <co.pascu@gmail.com>
2022-05-25 18:52:14 +0300

    fix: modified footer

--- src/theme/Footer/index.js
+++ src/theme/Footer/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Footer from '@theme-original/Footer';
-import ZypsyLogo from '../../../static/img/v0.3.0/logo-zypsy.svg';
+import ZypsyLogo from '../../../static/img/v0.4.0/logo-zypsy.svg';
 
 export default function FooterWrapper(props) {
   return (
     <>
       <Footer {...props} />
-      <a className='footerDesign' href='https://www.zypsy.com/' rel='noreferrer'>
+      <a className='footerDesign' href='https://www.zypsy.com/' target="_blank" rel='noreferrer'>
         <span>Designed by</span>
         <ZypsyLogo />
       </a>
