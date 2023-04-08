00b8f7f8e2dd129c95f8cbb3bc4f7dfc0d3f3a03
Sean Zhao <zhaoxiang2845@gmail.com>
2023-04-08 15:08:26 +0800

    fix: 404 protocol/technical-framework

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,43 +1,40 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
-require("dotenv").config();
+require('dotenv').config()
 
-const isDev = process.env.NODE_ENV === "development" ? true : false;
+const isDev = process.env.NODE_ENV === 'development' ? true : false
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const lightCodeTheme = require('prism-react-renderer/themes/github')
+const darkCodeTheme = require('prism-react-renderer/themes/dracula')
 
-const math = require("remark-math");
-const katex = require("rehype-katex");
+const math = require('remark-math')
+const katex = require('rehype-katex')
 
 /** @type {import("@docusaurus/types").Config} */
 const config = {
-  title: "CyberConnect Dev Center",
-  tagline: "Building the composable social graph protocol for Web3.",
-  url: "https://docs.cyberconnect.me",
-  baseUrl: "/",
-  onBrokenLinks: "warn",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
-  organizationName: "cyberconnecthq",
-  projectName: "cyberconnect-docs-v2",
+  title: 'CyberConnect Dev Center',
+  tagline: 'Building the composable social graph protocol for Web3.',
+  url: 'https://docs.cyberconnect.me',
+  baseUrl: '/',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'cyberconnecthq',
+  projectName: 'cyberconnect-docs-v2',
 
   presets: [
     [
-      "classic",
+      'classic',
       /** @type {import("@docusaurus/preset-classic").Options} */
       ({
         docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
+          sidebarPath: require.resolve('./sidebars.js'),
           editUrl: ({ docPath }) => {
-            const nextVersionDocsDirPath = "docs";
-            return `https://github.com/cyberconnecthq/cyberconnect-docs-v2/edit/main/${nextVersionDocsDirPath}/${docPath}`;
+            const nextVersionDocsDirPath = 'docs'
+            return `https://github.com/cyberconnecthq/cyberconnect-docs-v2/edit/main/${nextVersionDocsDirPath}/${docPath}`
           },
-          routeBasePath: "/",
-          remarkPlugins: [
-            [require("@docusaurus/remark-plugin-npm2yarn"), { sync: true }],
-            math,
-          ],
+          routeBasePath: '/',
+          remarkPlugins: [[require('@docusaurus/remark-plugin-npm2yarn'), { sync: true }], math],
           rehypePlugins: [katex],
           includeCurrentVersion: isDev,
           showLastUpdateTime: true,
@@ -48,35 +45,35 @@ const config = {
           blogDescription: 'CyberConnect Changelog',
           postsPerPage: 'ALL',
           routeBasePath: '/changelog',
-          showReadingTime: false
+          showReadingTime: false,
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+          customCss: require.resolve('./src/css/custom.css'),
         },
         gtag: {
-          trackingID: "G-9FMF2NF7NK",
+          trackingID: 'G-9FMF2NF7NK',
           anonymizeIP: true,
         },
         sitemap: {
-          changefreq: "weekly",
+          changefreq: 'weekly',
           priority: 0.5,
-          ignorePatterns: ["/tags/**"],
-          filename: "sitemap.xml",
+          ignorePatterns: ['/tags/**'],
+          filename: 'sitemap.xml',
         },
       }),
     ],
   ],
   plugins: [
     [
-      require.resolve("@edno/docusaurus2-graphql-doc-generator"),
+      require.resolve('@edno/docusaurus2-graphql-doc-generator'),
       {
-        schema: "https://api.cyberconnect.dev/testnet/",
-        rootPath: "./docs/",
-        baseURL: "/cyberconnect/reference/",
-        linkRoot: "/",
-        homepage: "./docs/cyberconnect/reference/graphql.md",
+        schema: 'https://api.cyberconnect.dev/testnet/',
+        rootPath: './docs/',
+        baseURL: '/cyberconnect/reference/',
+        linkRoot: '/',
+        homepage: './docs/cyberconnect/reference/graphql.md',
         loaders: {
-          UrlLoader: "@graphql-tools/url-loader",
+          UrlLoader: '@graphql-tools/url-loader',
         },
         diffMethod: false, // The method to be used for identifying changes in the schema for triggering the documentation generation. Set it to false to prevent from automatic updates.
       },
@@ -91,31 +88,35 @@ const config = {
           },
           {
             from: '/guides/recommendation/introduction',
-            to: '/api/recommendation/introduction'
+            to: '/api/recommendation/introduction',
           },
           {
             from: '/guides/recommendation/labels',
-            to: '/api/recommendation/labels'
+            to: '/api/recommendation/labels',
           },
           {
             from: '/api/connection/like-post',
-            to: '/api/connection/like-content'
+            to: '/api/connection/like-content',
           },
-        ]
-      }
-    ]
+          {
+            from: '/protocol/technical-framework',
+            to: '/',
+          },
+        ],
+      },
+    ],
   ],
   i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
+    defaultLocale: 'en',
+    locales: ['en'],
     localeConfigs: {
       en: {
-        htmlLang: "en-GB",
-        label: "English",
+        htmlLang: 'en-GB',
+        label: 'English',
       },
       ru: {
-        htmlLang: "ru-RU",
-        label: "Русский",
+        htmlLang: 'ru-RU',
+        label: 'Русский',
       },
     },
   },
@@ -123,48 +124,48 @@ const config = {
     /** @type {import("@docusaurus/preset-classic").ThemeConfig} */
     ({
       navbar: {
-        title: "",
+        title: '',
         logo: {
-          alt: "CyberConnect Logo",
-          src: "img/logo-custom.svg",
+          alt: 'CyberConnect Logo',
+          src: 'img/logo-custom.svg',
         },
         items: [
           {
-            type: "docSidebar",
-            position: "left",
-            sidebarId: "cyberconnectSidebar",
-            label: "CyberConnect",
+            type: 'docSidebar',
+            position: 'left',
+            sidebarId: 'cyberconnectSidebar',
+            label: 'CyberConnect',
           },
           {
-            type: "docSidebar",
-            position: "left",
-            sidebarId: "link3Sidebar",
-            label: "Link3",
+            type: 'docSidebar',
+            position: 'left',
+            sidebarId: 'link3Sidebar',
+            label: 'Link3',
           },
           {
-            href: "https://testnet.cyberconnect.me/",
-            label: "Testnet",
-            position: "right",
+            href: 'https://testnet.cyberconnect.me/',
+            label: 'Testnet',
+            position: 'right',
           },
           {
-            href: "https://dashboard.cyberconnect.me/",
-            label: "Dashboard",
-            position: "right",
+            href: 'https://dashboard.cyberconnect.me/',
+            label: 'Dashboard',
+            position: 'right',
           },
           {
-            href: "https://form.typeform.com/to/T2pjguoP",
-            label: "Feedback",
-            position: "right",
+            href: 'https://form.typeform.com/to/T2pjguoP',
+            label: 'Feedback',
+            position: 'right',
           },
           {
-            type: "docsVersionDropdown",
-            position: "right",
+            type: 'docsVersionDropdown',
+            position: 'right',
           },
           {
-            href: "https://github.com/cyberconnecthq/",
-            position: "right",
-            className: "header-github-link",
-            "aria-label": "GitHub repository",
+            href: 'https://github.com/cyberconnecthq/',
+            position: 'right',
+            className: 'header-github-link',
+            'aria-label': 'GitHub repository',
           },
           // Disabled translation
           { to: 'changelog', label: 'Changelog', position: 'right' },
@@ -175,7 +176,7 @@ const config = {
         ],
       },
       footer: {
-        style: "light",
+        style: 'light',
         links: [],
       },
       docs: {
@@ -188,7 +189,7 @@ const config = {
         darkTheme: darkCodeTheme,
       },
       colorMode: {
-        defaultMode: "light",
+        defaultMode: 'light',
         disableSwitch: false,
         respectPrefersColorScheme: false,
       },
@@ -199,32 +200,32 @@ const config = {
       metadata: [
         // Keywords
         {
-          name: "keywords",
-          content: "cyberconnect, protocol, social graph, blockchain, web3",
+          name: 'keywords',
+          content: 'cyberconnect, protocol, social graph, blockchain, web3',
         },
         // OG
-        { name: "og:title", content: "Developer Center | CyberConnect" },
+        { name: 'og:title', content: 'Developer Center | CyberConnect' },
         {
-          name: "og:description",
-          content: "Building the composable social graph protocol for Web3.",
+          name: 'og:description',
+          content: 'Building the composable social graph protocol for Web3.',
         },
-        { name: "og:type", content: "website" },
-        { name: "og:url", content: "https://docs.cyberconnect.me/" },
+        { name: 'og:type', content: 'website' },
+        { name: 'og:url', content: 'https://docs.cyberconnect.me/' },
         {
-          name: "og:image",
-          content: "https://docs.cyberconnect.me/img/ogimage.png",
+          name: 'og:image',
+          content: 'https://docs.cyberconnect.me/img/ogimage.png',
         },
         // Twitter
-        { name: "twitter:title", content: "Developer Center | CyberConnect" },
+        { name: 'twitter:title', content: 'Developer Center | CyberConnect' },
         {
-          name: "twitter:description",
-          content: "Building the composable social graph protocol for Web3.",
+          name: 'twitter:description',
+          content: 'Building the composable social graph protocol for Web3.',
         },
-        { name: "twitter:card", content: "summary_large_image" },
-        { name: "twitter:site", content: "@CyberConnectHQ" },
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:site', content: '@CyberConnectHQ' },
         {
-          name: "twitter:image",
-          content: "https://docs.cyberconnect.me/img/ogimage.png",
+          name: 'twitter:image',
+          content: 'https://docs.cyberconnect.me/img/ogimage.png',
         },
       ],
       algolia: {
@@ -233,27 +234,26 @@ const config = {
         // Public API key: it is safe to commit it
         apiKey: process.env.ALGOLIA_API_KEY,
         indexName: process.env.ALGOLIA_INDEX_NAME,
-        searchPagePath: "search",
+        searchPagePath: 'search',
         contextualSearch: true,
       },
     }),
   stylesheets: [
     {
-      href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
-      type: "text/css",
-      integrity:
-        "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
-      crossorigin: "anonymous",
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity: 'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
     },
   ],
   scripts: [
     // Plausible.io script
     {
-      src: "https://plausible.io/js/plausible.js",
+      src: 'https://plausible.io/js/plausible.js',
       defer: true,
-      "data-domain": "docs.cyberconnect.me",
+      'data-domain': 'docs.cyberconnect.me',
     },
   ],
-};
+}
 
-module.exports = config;
+module.exports = config
